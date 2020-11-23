ce7a2b19c204edb19a473fa96c554bceb99de961
roy De jong <roydejong@MacBook-Pro-van-roy.local>
2020-11-23 23:18:30 +0100

    updates 2.0 version

--- make_emails.py
+++ make_emails.py
@@ -8,8 +8,12 @@ names = ['james', 'john', 'robert', 'michael', 'william', 'david', 'richard', 'j
         'daniel', 'matthew', 'anthony', 'donald', 'mark', 'paul', 'steven', 'andrew','kenneth', 'joshua', 'kevin', 'brian', 'george', 'edward', 'ronald', 'timothy', 'jason', 'rebecca', 'laura',
         'emily', 'kimberly', 'betty', 'margaret', 'lisa', 'karen', 'jessica', 'susan','linda','jennifer',' patricia', 'mary', 'charlotte', 'kayla', 'isabelle', 'rose', 'natalie','diana']
 
+#last names  ( optional ) 
+last_names = ['smith', 'johnson','williams','jones','davis','brown','thomas','miller','wilson', 'moore']
+
 #Extensions 
-extension = ["@gmail.com", "@yahoo.com", "@live.com", "@live.nl", "@kpn.nl", "@ziggo.nl"]
+extension = ["@gmail.com", "@yahoo.com", "@live.com", "@live.nl", "@kpn.nl", "@ziggo.nl", 
+            "@live.com", "@aol.com", "@web.de", "@msn.com", "@hotmail.fr", "@hotmail.co.uk", "@orange.fr", "@msn.com"]
 
 #List with generated emails 
 generated = [] 
@@ -21,13 +25,15 @@ def create_emails():
         name_char = random.choice(names)
         extension_char = random.choice(extension)
         generated.append((str(name_char) + str(create_random_num(100)) + str(extension_char)))
-    
-    print("There were " + str(len(generated)) + " words generated!")
+
+
     show_email_list()
     remove_duplicates(generated)
+    write_to_file()
 
 #Shows the emails from the generated list 
 def show_email_list(): 
+    print("There were " + str(len(generated)) + " words generated!")
     for i in generated: 
         print(i)
 
@@ -41,6 +47,10 @@ def create_random_num(leng):
     n = random.randint(1,leng)
     return n 
 
+def write_to_file(): 
+    with open('generated_emails.txt', 'w') as f:
+        for item in generated:
+            f.write("%s\n" % item)
 
 
 #Runs first 
